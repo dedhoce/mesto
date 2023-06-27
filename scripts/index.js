5c34cc0963cf452b64f093b5c41bc587aadbe675
A.Melitskov <dedhoce@yandex.ru>
2023-06-27 19:57:24 +0300

    исправление ошибок: исправить код js, устранить критические замечания ревью

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const profileEditButton = document.querySelector('.profile__edit-button')
+const popupEditProfile = document.querySelector('.popup_edit_profile')
+const popupButtonClose = popupEditProfile.querySelector('.popup__button-close')
+const profileName = document.querySelector('.profile__name')
+const profileSubname = document.querySelector('.profile__subname')
+const popupProfileName = popupEditProfile.querySelector('.popup__input_type_name')
+const popupProfileSubname = popupEditProfile.querySelector('.popup__input_type_subname')
+const formEditProfile = popupEditProfile.querySelector('.form-edit-profile')
+
+const elementGroup = document.querySelectorAll('.element__group')
+    
+function addPopup() {    
+    popupProfileName.value = profileName.textContent;
+    popupProfileSubname.value = profileSubname.textContent;
+    popupEditProfile.classList.add('popup_status_active')
+};
+
+function closePopup () {
+    popupEditProfile.classList.remove('popup_status_active')
+};
+
+profileEditButton.addEventListener('click', addPopup)
+popupButtonClose.addEventListener('click', closePopup)
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = `${popupProfileName.value}`;
+    profileSubname.textContent = `${popupProfileSubname.value}`;
+
+    closePopup();
+};
+
+formEditProfile.addEventListener('submit', formSubmitHandler);
+
